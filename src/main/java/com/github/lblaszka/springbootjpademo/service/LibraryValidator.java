80af587c224c8c0baa52ca703e1ee24cb8d92c10
lblaszka <mlblaszka@gmail.com>
2019-03-08 22:55:58 +0100

    Refactory Library, Author Services

--- src/main/java/com/github/lblaszka/springbootjpademo/service/LibraryValidator.java
+++ src/main/java/com/github/lblaszka/springbootjpademo/service/LibraryValidator.java
@@ -2,7 +2,34 @@ package com.github.lblaszka.springbootjpademo.service;
 
 import com.github.lblaszka.springbootjpademo.domain.Library;
 
-public interface LibraryValidator
+import static com.github.lblaszka.springbootjpademo.config.LibraryConfig.LIBRARY_NAME_MIN_LENGTH;
+
+public class LibraryValidatorImpl implements DomainValidator<Library>
 {
-    boolean check( Library library );
+    @Override
+    public boolean toAdd( Library library )
+    {
+        if( library == null )
+            return false;
+        if( library.getName() == null || library.getName().length() < LIBRARY_NAME_MIN_LENGTH )
+            return false;
+        if( library.getId() != null && library.getId() != 0 )
+            return false;
+
+        return true;
+    }
+
+
+    @Override
+    public boolean toUpdate( Library library )
+    {
+        if( library == null )
+            return false;
+        if( library.getName() == null || library.getName().length() < LIBRARY_NAME_MIN_LENGTH )
+            return false;
+        if( library.getId() == null || library.getId() == 0 )
+            return false;
+
+        return false;
+    }
 }
